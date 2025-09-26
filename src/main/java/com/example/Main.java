b7aa6d96d37033af6799539fd6b71cd4a87b64ae
Linda Eskilsson <eskilsson.linda@hotmail.se>
2025-09-26 12:51:18 +0200

    try push

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -5,5 +5,8 @@ import com.example.api.ElpriserAPI;
 public class Main {
     public static void main(String[] args) {
         ElpriserAPI elpriserAPI = new ElpriserAPI();
+        System.out.println("Push Test");
     }
+
+
 }
